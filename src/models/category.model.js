852e394c0380fce68d0caebd42aa2a63e524a048
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2025-10-22 11:48:54 -0300

    rotas novas10

--- src/models/category.model.js
+++ src/models/category.model.js
@@ -3,11 +3,13 @@ import { Model, DataTypes } from 'sequelize';
 export default (sequelize) => {
   class Category extends Model {
     static associate(models) {
+      // Associação com Scorecard
       this.belongsTo(models.Scorecard, {
         foreignKey: 'scorecardId',
         as: 'scorecard',
       });
 
+      // Associação com Criterion
       this.hasMany(models.Criterion, {
         as: 'criteria',
         foreignKey: 'categoryId',
@@ -28,6 +30,7 @@ export default (sequelize) => {
       allowNull: false,
       validate: { notEmpty: true },
     },
+    // A coluna que está faltando no seu banco de dados atual
     order: {
       type: DataTypes.INTEGER,
       allowNull: false,
